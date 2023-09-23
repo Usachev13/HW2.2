886e06ed883368a6b0eeaf345312e8383ca7d0f5
Usachev13 <2893318@gmail.com>
2023-09-23 13:50:16 +0300

    corrected extra class

--- src/Car.java
+++ src/Car.java
@@ -1,10 +1,10 @@
 public class Car extends Vehicle{
-    private String modelName;
     public Car(String modelName, int wheelsCount) {
         super(modelName,wheelsCount);
     }
     public void checkEngine() {
         System.out.println("Проверяем двигатель");
     }
+
 }
 
--- src/Truck.java
+++ src/Truck.java
@@ -11,5 +11,7 @@ public class Truck extends Vehicle{
     public void checkTrailer() {
         System.out.println("Проверяем прицеп");
     }
+
+
 }
 
